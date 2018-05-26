c9d38cebb0c6c3c200ce41a657daa0b8e849a334
Nuno Neves <youknownuno@gmail.com>
2018-05-26 22:19:45 +0100

    changed the tests to multiples of 10

--- test/test.js
+++ test/test.js
@@ -1,18 +1,47 @@
 const assert = require('assert');
 const cards = require('../cards.js').units;
+const exp = require('../expFormulas.js');
 
-describe('cards', function() {
+
+describe('exp formulas', () => {
+	describe('exp easy', () => {
+		it(
+			'The function should return the same value',
+			() => assert.equal( exp.easy(99), 100)
+		);	
+	});
+
+	describe('exp medium', () => {
+		it(
+			'The function should return the same value',
+			() => assert.equal( exp.medium(99), 9801)
+		);		
+	});
+
+	describe('exp hard', () => {
+		it(
+			'The function should return the same value',
+			() => assert.equal( exp.hard(30), 1073741824)
+		);
+	});
+});
+
+
+describe('cards', () => {
 
 	for (let key in cards) {
+		let seq = cards[key].sequence;
+		
+		for (let i = 0; i < seq.length; i++) {
 
-		describe(key, function() {
-		  it('The formula should return the same values as the sequence', function() {
-		  		let seq = cards[key].sequence
-		  		for (var i = 0; i < seq.length; i++) {
-		  			assert.equal(cards[key].formula(i+1), seq[i]);
-		  		}
-		  });
-		});
+			describe(key, () => {
+			  it(
+			  	`${key} lvl ${(i+1) * 10}: function should return same value`,
+			  	() => assert.equal(cards[key].formula((i+1) * 10), seq[i])
+			  );
+			});
+
+		}	
 	}
 
 });
