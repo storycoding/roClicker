30e1e3b0b9bbe6e7057e90ea3fa46401e0575a66
Nuno Neves <youknownuno@gmail.com>
2018-05-27 01:20:44 +0100

    all cards tested up to lvl 50

--- cards.js
+++ cards.js
@@ -8,115 +8,115 @@ const round = function(number) {
 const units = {
   garm : {
     formula: (lvl) =>  (lvl * 5),
-    sequence: [ 50, 100, 150, 200, 250 ], // * 10
+    sequence: [ 50, 100, 150, 200, 250 ],
     exp: exp.easy
   },
 
   archangeling : {
     formula: (lvl) => (lvl * 5),
-    sequence: [ 250, 488, 717, 936, 1145 ], // * 10
+    sequence: [ 249, 488, 717, 936, 1145 ],
     exp: exp.easy
   },
 
   angeling : {
     formula: (lvl) =>  (lvl * 25),
-    sequence: [25, 50, 75, 100, 125], // CHECK
+    sequence: [249, 488, 717, 936, 1145],
     exp: exp.easy
   },
 
   detale : {
     formula: (lvl) => (lvl * 50),
-    sequence: [50, 100, 150, 200, 250], // CHECK
+    sequence: [500, 1000, 1500, 2000, 2500],
     exp: exp.easy
   },
 
   baphomet : {
     formula: (lvl) => (lvl * 20),
-    sequence: [20, 40, 60, 80, 100], // CHECK
+    sequence: [200, 400, 600, 800, 1000],
     exp: exp.easy
   },
 
   doppelganger : {
     formula: (lvl) => (lvl * 15),
-    sequence: [15, 30, 45, 60, 75],  // CHECK
+    sequence: [150, 300, 450, 600, 750], 
     exp: exp.easy
   },
 
   ifrit : {
     formula: (lvl) => (lvl * 2),
-    sequence: [20, 40, 60, 80, 100], // * 10
+    sequence: [20, 40, 60, 80, 100],
     exp: exp.easy
   },
 
   phreeoni: {
     formula: (lvl) => round(lvl * 0.24 - (lvl / 24)),
-    sequence: [ 2.37, 4.53, 6.47, 8.24, 9.83 ], // * 10
+    sequence: [ 2.37, 4.53, 6.47, 8.24, 9.83 ],
     exp: exp.medium
   },
 
   eddga: {
     formula: (lvl) => round(lvl * 0.16 - (lvl / 10)),
-    sequence: [ 0.90, 1.64, 2.25, 2.75, 3.16 ], // * 10
+    sequence: [ 0.90, 1.64, 2.25, 2.75, 3.16 ],
     exp: exp.medium
   },
 
   drake: {
     formula: (lvl) => (lvl * 3.79),
-    sequence: [ 37.62, 74.50, 110.64, 146.07, 180.80 ], // * 10
+    sequence: [ 37.62, 74.50, 110.64, 146.07, 180.80 ],
     exp: exp.medium
   },
 
   beelzebub: {
     formula: (lvl) => (lvl * 1.92),
-    sequence: [ 17.17, 30.41, 40.61, 48.49, 54.56 ], // * 10
+    sequence: [ 17.17, 30.41, 40.61, 48.49, 54.56 ],
     exp: exp.medium
   },
 
   bacsojin: {
     formula: (lvl) => (lvl * 1),
-    sequence: [ 8.64, 14.80, 19.18, 22.30, 24.51 ], // * 10
+    sequence: [ 8.64, 14.80, 19.18, 22.30, 24.51 ],
     exp: exp.medium
   },
 
   moonlight : {
     formula: (lvl) => (lvl * 0.32),
-    sequence: [ 3.04, 5.72, 8.07, 10.13, 11.94 ], // * 10
+    sequence: [ 3.04, 5.72, 8.07, 10.13, 11.94 ],
     exp: exp.medium
   },
 
   mistress: {
     formula: (lvl) => (lvl * 1),
-    sequence: [ 9.06, 16.48, 22.55, 27.53, 31.60 ], // * 10
+    sequence: [ 9.06, 16.48, 22.55, 27.53, 31.60 ],
     exp: exp.medium
   },
 
    atroce: {
     formula: (lvl) => (lvl * 0.01),
-    sequence: [0.10, 0.20, 0.30, 0.40, 0.50], // * 10
+    sequence: [0.10, 0.20, 0.30, 0.40, 0.50],
     exp: exp.medium
   },
 
   lordOfDeath: {
     formula: (lvl) => (lvl * 5),
-    sequence: [50, 100, 140, 180, 210], // * 10
+    sequence: [50, 100, 140, 180, 210],
     exp: exp.medium
   },
 
   maya: {
     formula: (lvl) => (lvl * 0.19),
-    sequence: [ 1.90, 3.62, 5.18, 6.59, 7.86 ], // * 10
+    sequence: [ 1.90, 3.62, 5.18, 6.59, 7.86 ],
     exp: exp.medium
   },
 
   thanatos: {
     formula: (lvl) => (lvl * 0.99),
-    sequence: [ 9.06, 16.48, 22.55, 27.53, 31.60 ], // * 10
+    sequence: [ 9.06, 16.48, 22.55, 27.53, 31.60 ],
     exp: exp.medium
   },
 
   humans: {
     formula: (lvl) => (lvl * 2),
-    sequence: [2, 4, 6, 8, 10],
+    sequence: [20, 40, 60, 80, 100],
     exp: exp.hard
   }
 
